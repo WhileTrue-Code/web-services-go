413bb246d09b7a1954c9425127673e965a6e1ce1
unknown <popovictodor01@gmail.com>
2022-04-20 18:08:27 +0200

    Added decoder and encoder for json and uuid

---
+++ WebServices/helper.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+func decodeBody(r io.Reader) (*Config, error) {
+	dec := json.NewDecoder(r)
+	dec.DisallowUnknownFields()
+
+	var cfg Config
+	if err := dec.Decode(&cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+func renderJSON(w http.ResponseWriter, v interface{}) {
+	js, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func createId() string {
+	return uuid.New().String()
+}
