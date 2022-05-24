906497b600c7f69d439efefd1bd1f5b95c656467
Filip Siljic <fsiljic13@gmail.com>
2022-05-24 21:31:21 +0200

    extended helper and database update

--- WebServices/database/database.go
+++ WebServices/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -25,4 +26,44 @@ func New() (*Database, error) {
 	return &Database{
 		cli: client,
 	}, nil
+}
+
+func (db *Database) Config(config *Config) (*Config, error) {
+	kv := db.cli.KV()
+
+	dbkey, id := generateKey(config.Id, config.Version)
+	config.Id = id
+
+	data, err := json.Marshal(config)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &api.KVPair{Key: dbkey, Value: data}
+	_, err = kv.Put(c, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func (db *Database) Group(group *Group) (*Group, error) {
+	kv := db.cli.KV()
+
+	dbkey, id := generateKey(group.Id, group.Version)
+	group.Id = id
+
+	data, err := json.Marshal(group)
+	if err != nil {
+		return nil, err
+	}
+
+	g := &api.KVPair{Key: dbkey, Value: data}
+	_, err = kv.Put(g, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return group, nil
 }
