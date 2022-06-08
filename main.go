1456d65da00f1bb75bba6420caefa1971e9f8927
nananeko1305 <nananeko1305@hotmail.com>
2022-06-08 17:36:14 +0200

    Volim nissan gtr

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 
 	log.Println("service shutting down ...")
 
-	// gracefully stop serverrrr
+	// gracefully stop serverrrrr
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
