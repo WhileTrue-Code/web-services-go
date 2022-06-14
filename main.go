f98014e4c8961a8dd1051b407621bd64cd6cef25
nananeko1305 <nananeko1305@hotmail.com>
2022-06-14 14:11:29 +0200

    Odbrana

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 
 	log.Println("service shutting down ...")
 
-	// gracefully stop serverr
+	// gracefully stop serverrrrrrrrr
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
